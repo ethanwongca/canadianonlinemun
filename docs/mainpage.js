b019923c1f4246eac2413232012b08e4fbacdd3e
Ethan Wong <ethanwongca@gmail.com>
2024-08-12 17:29:48 -0700

    Found the updated website files from a couple years ago

--- docs/mainpage.js
+++ docs/mainpage.js
@@ -1,4 +1,4 @@
 const joinButton = document.getElementById('join');
 joinButton.addEventListener('click', function() {
-window.location.href = 'https://linktr.ee/canadianonlinemun';
+    window.location.href = 'https://linktr.ee/canadianonlinemun';
 });
